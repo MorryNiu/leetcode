b760bd7fc51fcace32a51af7e2d4fb61df1b65b7
Plutoic <morryniu123@gmail.com>
2018-01-03 13:55:20 +0800

    1/3

--- Algorithms/Find All Numbers Disappeared in an Array.c
+++ Algorithms/Find All Numbers Disappeared in an Array.c
@@ -3,12 +3,27 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* findDisappearedNumbers(int* nums, int numsSize, int* returnSize) {
-    int* res = (int*)malloc(numsSize*sizeof(int));
+    int* res = (int*)malloc((numsSize)*sizeof(int));
 
-    if (numsSize=0) {
+    memset(res, 0, numsSize*sizeof(int));
+
+    if (numsSize==0) {
+        returnSize = 0;
         return res;
     }
 
-    
+    for (size_t i = 0; i < numsSize; i++) {
+        res[nums[i]-1] = 1;
+    }
 
+    *returnSize = 0;
+
+    for (size_t i = 0; i < numsSize; i++) {
+        if (res[i]==0) {
+            res[(*returnSize)] = i+1;
+            (*returnSize) += 1;
+        }
+    }
+
+    return res;
 }
